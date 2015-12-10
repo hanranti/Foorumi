c05bd304fa9acb6eaa0a3aced80a299c97d28f97
Hannu <hannu.rantinoja@helsinki.fi>
2015-12-10 16:41:40 +0200

    topics

--- routes/topics.js
+++ routes/topics.js
@@ -7,35 +7,41 @@ var Models = require('../models');
 // Huom! Kaikki polut alkavat polulla /topics
 
 // GET /topics
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(findAll());
-    res.send(200);
+    Models.Topic.findAll().then(function (topics) {
+        res.json(topics);
+    });
 });
 
 // GET /topics/:id
-router.get('/:id', function(req, res, next) {
-  // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
-  var topicId = req.params.id;
-  res.send(200);
+router.get('/:id', function (req, res, next) {
+    // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
+    var topicId = req.params.id;
+    Models.Topic.findOne(topicId).then(function (topic) {
+        res.json(topic);
+    });
 });
 
 // POST /topics
-router.post('/', function(req, res, next) {
-  // Lisää tämä aihealue
-  var topicToAdd = req.body;
-  // Palauta vastauksena lisätty aihealue
-  res.send(200);
+router.post('/', function (req, res, next) {
+    // Lisää tämä aihealue
+    var topicToAdd = req.body;
+    Models.Topic.create({name: topicToAdd.name, description: topicToAdd.description})
+            .then(function (topic) {
+                res.json(topic);
+            });
+    // Palauta vastauksena lisätty aihealue
 });
 
 // POST /topics/:id/message
-router.post('/:id/message', function(req, res, next) {
-  // Lisää tällä id:llä varustettuun aihealueeseen...
-  var topicId = req.params.id;
-  // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
-  var messageToAdd = req.body;
-  // Palauta vastauksena lisätty viesti
-  res.send(200);
+router.post('/:id/message', function (req, res, next) {
+    // Lisää tällä id:llä varustettuun aihealueeseen...
+    var topicId = req.params.id;
+    // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
+    var messageToAdd = req.body;
+    // Palauta vastauksena lisätty viesti
+    res.send(200);
 });
 
 module.exports = router;
