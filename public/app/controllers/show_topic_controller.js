35377454ea6fa0d459023d8e87c0f881d1f36e6e
Rantinoja Hannu <hanranti@da5-cs-bk107-34.helsinki.fi>
2015-12-11 15:42:31 +0200

    tehtava 51

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,3 +1,9 @@
-FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('ShowTopicController', function ($scope, $routeParams, $location, Api) {
+    // Toteuta kontrolleri tähän
+    $scope.getTopic = Api.getTopic($routeParams.id)
+            .success(function (data, status, headers, config) {
+                $scope.topic = data;
+            }).
+            error(function (data, status, headers, config) {
+            });
 });
--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,12 @@
-FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
+FoorumApp.controller('TopicsListController', function ($scope, $location, Api) {
+    // Toteuta kontrolleri tähän
+    $scope.getTopics = Api.getTopics().success(function (data, status, headers, config) {
+        $scope.topics = data;
+    }).error(function (data, status, headers, config) {
+    });
+    $scope.addTopic = function () {
+        Api.addTopic($scope.newTopic).success(function (data, status, headers, config) {
+            $location.path("/topics/" + data.id);
+        });
+    };
 });
